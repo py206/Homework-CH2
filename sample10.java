3434dba3d9ad0e50504d749a397a5f666c5ca7ad
py206 <penny880206@gmail.com>
2019-10-08 19:42:46 +0800

    first commit

---
+++ sample10.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class sample10 
+{
+	public static void main(String[] args)throws IOException
+	{
+		for (int i=1;i<=5;i++)
+		{
+		System.out.println("第"+i+"次的迴圈");
+		}
+		
+		System.out.println("迴圈結束");
+	}
+}
+
+
---
+++ sample12.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class sample12
+{
+	public static void main(String[] args)throws IOException
+	{
+		int i=1;
+		do
+		{
+		System.out.println("第"+i+"次的迴圈");
+		i++;
+		}
+		while(i<=5);
+		System.out.println("迴圈結束");
+	}
+}
